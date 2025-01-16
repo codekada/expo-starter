09d3757af19e888791c33d3a93fa79c02dfc88c8
Jeck <zrkeimperial@gmail.com>
2025-01-16 23:29:21 +0800

    ver0.2.2

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,20 +1,41 @@
 import React from "react";
-import { ScrollView, TextInput } from "react-native";
+import { Button, ScrollView, TextInput } from "react-native";
 import { Text, View } from "@/components/Themed";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+    SafeAreaView,
+    SafeAreaProvider,
+    SafeAreaInsetsContext,
+    useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 export default function Home() {
+    console.log("App started");
     return (
         <>
-            <SafeAreaView className="h-[350px]">
-                <View className="flex-1 h-[350px] text-[30px] border items-center justify-center bg-[#282c34]">
-                    <Text className="text-xl">asdaadssdsasds</Text>
+            <SafeAreaView>
+                <View className="h-[350px] flex justify-center items-center gap-[10px]">
+                    <Text>Hello world!</Text>
+                    <View className="flex-row justify-center gap-[5px]">
+                        <Button title="Hey" />
+                        <Button title="Hello?" />
+                    </View>
                 </View>
+                <ScrollView>
+                    <View className="h-[350px]">
+                        <Text className="text-4xl">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore
+                            eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </Text>
+                    </View>
+                </ScrollView>
             </SafeAreaView>
-            <ScrollView className="h-[350px]">
-                <Text>asdasdsdasdasd</Text>
-                <Text className="h-[300px]">asdasdsdasdasd</Text>
-            </ScrollView>
         </>
     );
 }
